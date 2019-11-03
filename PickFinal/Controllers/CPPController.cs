bb5b718480817ce553895a21a2006846ff96a56d
Specer Krug <spncrkrug@gmail.com>
2019-11-03 18:23:25 -0500

    Master

---
+++ PickFinal/Controllers/CPPController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PickFinal.Controllers
+{
+    public class CPPController : Controller
+    {
+        // GET: CPP
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PickFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PickFinal.Startup))]
+namespace PickFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
