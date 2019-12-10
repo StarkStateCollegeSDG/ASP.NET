047422d724a99b6d65d8eeef02ae2b7477f70f47
Spencer Krug <spncrkrug@gmail.com>
2019-12-10 18:35:59 -0500

    Restarted project.

---
+++ MoroskoWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoroskoWebsite.Startup))]
+namespace MoroskoWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
