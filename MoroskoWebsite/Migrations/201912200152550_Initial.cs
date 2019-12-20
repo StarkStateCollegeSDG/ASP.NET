1f0d9852062148a091731237d597855a803a1a75
Specer Krug <spncrkrug@gmail.com>
2019-12-19 21:06:46 -0500

    Admin role created and added to a new user.

---
+++ MoroskoWebsite/Migrations/201912200152550_Initial.cs
@@ -0,0 +1,256 @@
+﻿namespace MoroskoWebsite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            //Commented this out so whenever we run update-database it does not try to 
+            //re-create the database.
+            /*
+            CreateTable(
+                "dbo.AdvCPPs",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        projectname = c.String(),
+                        description = c.String(),
+                        studentname = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.AdvVBs",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        projectname = c.String(),
+                        description = c.String(),
+                        studentname = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        coursename = c.String(),
+                        coursegrade = c.String(),
+                        finalId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Finals", t => t.finalId, cascadeDelete: true)
+                .Index(t => t.finalId);
+            
+            CreateTable(
+                "dbo.Finals",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        finalname = c.String(),
+                        finalgrade = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.UserCourses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        aspnetusersId = c.String(),
+                        courseId = c.Int(nullable: false),
+                        AspNetUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers1", t => t.AspNetUser_Id)
+                .ForeignKey("dbo.Courses", t => t.courseId, cascadeDelete: true)
+                .Index(t => t.courseId)
+                .Index(t => t.AspNetUser_Id);
+            
+            CreateTable(
+                "dbo.AspNetUsers1",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.AspNetRoles1",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.AspNetUserClaims1",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                        AspNetUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers1", t => t.AspNetUser_Id)
+                .Index(t => t.AspNetUser_Id);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins1",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        LoginProvider = c.String(),
+                        ProviderKey = c.String(),
+                        UserId = c.String(),
+                        AspNetUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers1", t => t.AspNetUser_Id)
+                .Index(t => t.AspNetUser_Id);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetRoleAspNetUsers",
+                c => new
+                    {
+                        AspNetRole_Id = c.String(nullable: false, maxLength: 128),
+                        AspNetUser_Id = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.AspNetRole_Id, t.AspNetUser_Id })
+                .ForeignKey("dbo.AspNetRoles1", t => t.AspNetRole_Id, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers1", t => t.AspNetUser_Id, cascadeDelete: true)
+                .Index(t => t.AspNetRole_Id)
+                .Index(t => t.AspNetUser_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.UserCourses", "courseId", "dbo.Courses");
+            DropForeignKey("dbo.UserCourses", "AspNetUser_Id", "dbo.AspNetUsers1");
+            DropForeignKey("dbo.AspNetUserLogins1", "AspNetUser_Id", "dbo.AspNetUsers1");
+            DropForeignKey("dbo.AspNetUserClaims1", "AspNetUser_Id", "dbo.AspNetUsers1");
+            DropForeignKey("dbo.AspNetRoleAspNetUsers", "AspNetUser_Id", "dbo.AspNetUsers1");
+            DropForeignKey("dbo.AspNetRoleAspNetUsers", "AspNetRole_Id", "dbo.AspNetRoles1");
+            DropForeignKey("dbo.Courses", "finalId", "dbo.Finals");
+            DropIndex("dbo.AspNetRoleAspNetUsers", new[] { "AspNetUser_Id" });
+            DropIndex("dbo.AspNetRoleAspNetUsers", new[] { "AspNetRole_Id" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.AspNetUserLogins1", new[] { "AspNetUser_Id" });
+            DropIndex("dbo.AspNetUserClaims1", new[] { "AspNetUser_Id" });
+            DropIndex("dbo.UserCourses", new[] { "AspNetUser_Id" });
+            DropIndex("dbo.UserCourses", new[] { "courseId" });
+            DropIndex("dbo.Courses", new[] { "finalId" });
+            DropTable("dbo.AspNetRoleAspNetUsers");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.AspNetUserLogins1");
+            DropTable("dbo.AspNetUserClaims1");
+            DropTable("dbo.AspNetRoles1");
+            DropTable("dbo.AspNetUsers1");
+            DropTable("dbo.UserCourses");
+            DropTable("dbo.Finals");
+            DropTable("dbo.Courses");
+            DropTable("dbo.AdvVBs");
+            DropTable("dbo.AdvCPPs");
+            */
+        }
+    }
+}
---
+++ MoroskoWebsite/Migrations/Configuration.cs
@@ -0,0 +1,43 @@
+﻿namespace MoroskoWebsite.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using MoroskoWebsite.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MoroskoWebsite.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MoroskoWebsite.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+            //Create the role of admin and assign it to a new user.
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+            if (!context.Users.Any(u => u.UserName == "admin@domain.com"))
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = "Admin",
+                    Email = "admin@domain.com"
+                };
+                IdentityResult result = userManager.Create(user, "K@pp@ Th3t@");
+                context.Roles.AddOrUpdate(r => r.Name, new IdentityRole { Name = "Admin" });
+                context.SaveChanges();
+                userManager.AddToRole(user.Id, "Admin");
+                context.SaveChanges();
+            }
+        }
+    }
+}
