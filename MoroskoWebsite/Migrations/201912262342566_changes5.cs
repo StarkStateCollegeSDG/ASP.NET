efc200a66f91e448bf9686f2b4eae997384d3a76
Specer Krug <spncrkrug@gmail.com>
2019-12-26 18:46:34 -0500

    Save point.

---
+++ MoroskoWebsite/Migrations/201912262342566_changes5.cs
@@ -0,0 +1,45 @@
+﻿namespace MoroskoWebsite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changes5 : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.AspNetRoleAspNetUsers", "AspNetRole_Id", "dbo.AspNetRoles");
+            DropForeignKey("dbo.AspNetRoleAspNetUsers", "AspNetUser_Id", "dbo.AspNetUsers");
+            DropIndex("dbo.AspNetRoleAspNetUsers", new[] { "AspNetRole_Id" });
+            DropIndex("dbo.AspNetRoleAspNetUsers", new[] { "AspNetUser_Id" });
+            AddColumn("dbo.AspNetUsers", "AspNetRole_Id", c => c.String(maxLength: 128));
+            AddColumn("dbo.AspNetRoles", "AspNetUser_Id", c => c.String(maxLength: 128));
+            CreateIndex("dbo.AspNetUsers", "AspNetRole_Id");
+            CreateIndex("dbo.AspNetRoles", "AspNetUser_Id");
+            AddForeignKey("dbo.AspNetUsers", "AspNetRole_Id", "dbo.AspNetRoles", "Id");
+            AddForeignKey("dbo.AspNetRoles", "AspNetUser_Id", "dbo.AspNetUsers", "Id");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.AspNetRoleAspNetUsers",
+                c => new
+                    {
+                        AspNetRole_Id = c.String(nullable: false, maxLength: 128),
+                        AspNetUser_Id = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.AspNetRole_Id, t.AspNetUser_Id });
+            
+            DropForeignKey("dbo.AspNetRoles", "AspNetUser_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUsers", "AspNetRole_Id", "dbo.AspNetRoles");
+            DropIndex("dbo.AspNetRoles", new[] { "AspNetUser_Id" });
+            DropIndex("dbo.AspNetUsers", new[] { "AspNetRole_Id" });
+            DropColumn("dbo.AspNetRoles", "AspNetUser_Id");
+            DropColumn("dbo.AspNetUsers", "AspNetRole_Id");
+            CreateIndex("dbo.AspNetRoleAspNetUsers", "AspNetUser_Id");
+            CreateIndex("dbo.AspNetRoleAspNetUsers", "AspNetRole_Id");
+            AddForeignKey("dbo.AspNetRoleAspNetUsers", "AspNetUser_Id", "dbo.AspNetUsers", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.AspNetRoleAspNetUsers", "AspNetRole_Id", "dbo.AspNetRoles", "Id", cascadeDelete: true);
+        }
+    }
+}
--- MoroskoWebsite/Models/AspNetUser.cs
+++ MoroskoWebsite/Models/AspNetUser.cs
@@ -37,6 +37,7 @@ namespace MoroskoWebsite.Models
         public bool LockoutEnabled { get; set; }
         public int AccessFailedCount { get; set; }
         public string UserName { get; set; }
+        public virtual AspNetRole AspNetRole { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<AspNetUserClaim> AspNetUserClaims { get; set; }
