c85025a6e727c0c8d6b53d1e3701a7447187fb38
Specer Krug <spncrkrug@gmail.com>
2019-12-26 10:22:09 -0500

    Added a migration used for testing possible changes.

---
+++ MoroskoWebsite/Migrations/201912261516282_changes.cs
@@ -0,0 +1,21 @@
+﻿namespace MoroskoWebsite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changes : DbMigration
+    {
+        /* Always use this to test for existing changes
+         * before updating the database.
+         * Up() is everything being added.
+         * Down() is everything being deleted.
+         */
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
